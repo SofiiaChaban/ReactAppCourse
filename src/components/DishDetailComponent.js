9ac0a87c3fcfba02c267955c02366b231486c27d
Sofiia Chaban <43277552+SofiiaChaban@users.noreply.github.com>
2020-09-20 17:35:35 +0300

    post comment to server

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -32,7 +32,7 @@ class CommentForm extends Component{
 
     handleSubmitChange(values){
         this.toggleModal();
-        this.props.addComment(this.props.dishId,values.rating,values.name,values.comment)
+        this.props.postComment(this.props.dishId,values.rating,values.name,values.comment)
     }
 
     render(){
@@ -122,7 +122,7 @@ class CommentForm extends Component{
         }
     }
 
-    function RenderComments({ comments, addComment,dishId }) {
+    function RenderComments({ comments, postComment,dishId }) {
         if (!comments || comments.length === 0) {
           return (
             <div></div>
@@ -143,7 +143,7 @@ class CommentForm extends Component{
               <ul className="list-unstyled">
                   {c}
               </ul>
-              <CommentForm dishId={dishId} addComment={addComment}/>
+              <CommentForm dishId={dishId} postComment={postComment}/>
              </div>
           );
         }
@@ -190,7 +190,7 @@ class CommentForm extends Component{
                             </div>
                             <div className="col-12 col-md-5 m-1">
                             <RenderComments comments={props.comments}
-                            addComment = {props.addComment}
+                            postComment = {props.postComment}
                             dishId={props.dish.id}/>
                             </div>
                     </div>
